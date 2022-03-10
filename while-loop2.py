8eed78de882e7514684c5b594c4a9aef9150746e
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-03-09 23:42:07 -0600

    Add files via upload

---
+++ while-loop2.py
@@ -0,0 +1,31 @@
+my_list = [1,2,3]
+for item in [1,2,3]:
+    print(item)
+
+i = 0
+while i < len([1,2,3]):
+    print(i)
+    i += 1
+
+i = 0
+while i < len(my_list):
+    print(my_list[i])
+    i += 1
+
+##while loop
+while True:
+    input('say something: ')
+    break
+
+# while loop continuation
+# while True:
+#     input('say something: ')
+    
+# while loop
+while True:
+    response = input('say something: ')
+    if (response == 'bye'):
+        break
+
+    
+
